35291af10da3c226bf5682ddce09e82ff4e0d2be
bl2i4n <bl2i4n@yahoo.com>
2020-05-04 00:58:27 -0700

    could not get data from forms :(

--- src/app/components/question-items/question-items.component.ts
+++ src/app/components/question-items/question-items.component.ts
@@ -12,40 +12,5 @@ export class QuestionItemsComponent implements OnInit {
 
   ngOnInit(): void { 
   }
-  
-  // onSubmit(form){
-  //   console.log("clicked")
-  //   this.testValue = form;
-  //   console.log(this.testValue);
-  //   // console.log(form.controls['Yes'].value)
-  //   // console.log(form.controls['Yes']);
-  //   // console.log(form.controls['No']);
-  //   // console.log(form.controls['idk']);
-  //   if (form.controls['Yes'].value === 1) {
-  //     console.log("yay found 1")
-  //   } else if (form.controls['No'].value === 2) {
-  //     console.log("yay found 2")
-  //   } else if (form.controls['idk'].value === 2) {
-  //     console.log("yay found 2")
-  //   } else {
-  //     console.log("nothing found")
-  //   }
-  // }
-  userAnswer: Number;
-  sum: Number;
-
-  log(x) {
-    // console.log(x.value)
-    this.userAnswer = x.value
-    // this.userAnswer = x.name;
-    console.log(this.userAnswer)
-    return this.userAnswer;
-  }
-
-  handleClick() {
-    this.log(this.userAnswer);
-    console.log("userAnswer is", this.userAnswer)
-  }
-
 
 }
